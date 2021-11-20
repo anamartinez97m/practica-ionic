f73de4a19eb5d76284fd27217ce0edc06ef8244e
anamartinez97m <anamartinez97m@gmail.com>
2021-11-20 11:43:19 +0100

    Modified travels page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { TabsComponent } from '../components/tabs/tabs.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,9 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    TabsComponent
+  ]
 })
 export class HomePageModule {}
